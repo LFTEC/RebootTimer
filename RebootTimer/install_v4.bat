e334034423caaa9ed0fe4a7eba23c8e0a2b4f57c
chris <jcpuff20@gmail.com>
2024-06-23 16:42:19 +0800

    date: 2024-6-23 解决.net 4.0检测是否存在的BUG

--- RebootTimer/install_v4.bat
+++ RebootTimer/install_v4.bat
@@ -3,7 +3,7 @@ setlocal
 
 REM check and install .NET Framework 4.0
 echo check whether .NET Framework 4.0 is  installed...
-reg query "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4" /v Install >nul 2>&1
+reg query "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v Install >nul 2>&1
 if %errorlevel% neq 0 (
     echo .NET Framework 4.0 not exist, installing...
     REM 请确保 .NET Framework 安装包的路径正确
